e8717731f7db09b4b23fa7ed915877b36a40ea81
Roy <roynny51@gmail.com>
2020-04-30 12:52:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My hello World
+        <FirstComponent></FirstComponent>
+      </div>
+    );
+  }
+}
+
+
+
+// class component
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstComponent">
+        FirstComponent
+      </div>
+    );
+  }
+}
+
+export default App;
